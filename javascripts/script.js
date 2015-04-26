b9d9492d8bc1bee46ab1003881cc97858f5c9e4b
Edmund Lo <edmund.lo@wishabi.com>
2015-04-26 16:44:14 -0400

    Restructure code a little

---
+++ javascripts/script.js
@@ -0,0 +1,20 @@
+//scrolling animation script
+
+Animator = function() {
+  var initialize = function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+      }
+    });
+  };
+}
+
